c6970811c4a00ffd93a419c3e81b5e560e195942
migueldev2006 <gironorzcoluismiguel3@gmail.com>
2024-06-04 14:51:28 -0500

    solucion al 5 ejercicio de php

--- ejercicios/5.php
+++ ejercicios/5.php
@@ -2,8 +2,15 @@
 
 function FierroAlv(){
     for ($i=0; $i < 101; $i++) { 
-        if ($i % 3 == 0)
-        echo "Peso";
-        elseif ($i % 5 == 0)
+        if ($i % 3 == 0){
+        echo "Peso\n";
+        }if ($i % 5 == 0){
+        echo "Pluma\n";
+        }if ($i % 3 == 0 && $i %5 == 0){
+        echo "PesoPluma\n";
+        }
+        
     }
 }
+
+echo FierroAlv();
