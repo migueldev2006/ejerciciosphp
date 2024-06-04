257b1b17bba712e80bdcd685c88396d56c692bce
migueldev2006 <gironorzcoluismiguel3@gmail.com>
2024-06-04 16:05:13 -0500

    Cargo a solucion del 4 ejercicio, ademas paa este emismo se empleo el convertir los numeros a string para luego alamcenarla en un array, utilizamos strval y str_split. Luego cuando empleamos el ciclo para realizar la suma de dichos digitos  debemos especificar que son enteros mediante intval.

--- ejercicios/4.php
+++ ejercicios/4.php
@@ -1,9 +1,14 @@
 <?php
-/*
+
 function sumaNumeroEntero(){
-    $entero = intval(readline("ingresa un numero entero: "));
-    
-}*/
+    $entero = intval(readline("Ingresa un numero entero: "));
+    $texto = strval($entero);
+    $digitoEntero = str_split($texto);
 
-
-echo $num;
+    $suma = 0;
+    for ($i = 0; $i < count($digitoEntero); $i++){
+        $suma += intval($digitoEntero[$i]);
+    }
+    echo "La suma de los digitos es: $suma";
+}
+echo sumaNumeroEntero();
